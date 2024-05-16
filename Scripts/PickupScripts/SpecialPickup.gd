f3e43c30ea8b933110305334b39f8afd63076619
Matthieu Benedict <97464794+gdmbenedict@users.noreply.github.com>
2024-05-16 13:13:58 -0300

    Added Random Pickup
    Added a random special pickup spawner called special pickup.

---
+++ Scripts/PickupScripts/SpecialPickup.gd
@@ -0,0 +1,15 @@
+extends Node
+
+@export_category("Pickups")
+@export var pickups: Array[PackedScene]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	
+	#Spawn pickup and set location and parent
+	var spawnedPickup = pickups.pick_random().instantiate() as Node3D
+	spawnedPickup.global_position = self.global_position
+	self.get_parent().add_child(spawnedPickup)
+	
+	#despawn self
+	queue_free()
